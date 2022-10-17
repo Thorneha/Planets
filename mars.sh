0ec42439850596d1a8d52a041678358f01927380
Hayden Thorne <hayden.thorne@vuw.ac.nz>
2022-10-17 15:08:07 +1300

    added new functionality

--- mars.sh
+++ mars.sh
@@ -3,3 +3,4 @@
 echo "Cold and dry, but everything is my favourite colour"
 echo "the two moons may be a problem for Wolfman"
 echo "But the mummy will appreciate the lack of humidity"
+Error is here
